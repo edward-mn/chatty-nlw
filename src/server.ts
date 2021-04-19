b4af200e8bbf8661e901980dd5b3832676da48ea
edward-mn <edward.moreira@yahoo.com>
2021-04-19 20:45:12 -0300

    Create chatty project :boy::thought_balloon::man_technologist:

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+// ... significa que é necessário fazer a instalação da tipagem - yarn add @types/nome_biblioteca -D (-D = ambiente Dev)
+import express from 'express'; 
+
+const app = express();
+
+/* 
+ - GET = Buscas
+ - POST = Criação
+ - PUT = Alteração
+ - PATCH = Alterar uma informação específica
+ - DELETE = Deletar
+*/
+
+// Podemos utilizar a mesma rota por conta dos métodos HTTP
+app.get('/', (req, res) => {
+  //return res.send("Olá NLW 05");
+  return res.json({
+    message: "Olá NLW 05"
+  });
+});
+
+app.post('/', (req, res) => {
+  return res.json({message: "Usuário salvo com sucesso!"});
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
+
+// yarn tsc --init (Arquivo de config TS)
+// Code missaoespacial
